4544fff836a0b29f01507a07d8e72c034f22542b
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-01-12 19:14:10 +0000

    palidrome checker basic complete

--- palidrome_checker.py
+++ palidrome_checker.py
@@ -1,6 +1,14 @@
-def check_palidrome():
-    pass
+def check_palidrome(word: str) -> bool:
+    top = len(word) - 1
+    bottom = 0
+    while top > bottom:
+        if word[top] != word[bottom]:
+            return False
+        top -= 1
+        bottom += 1
+    return True
 
 
 if __name__ == "__main__":
-    print("hello")
+    user_input = input("Enter a palidrome: ")
+    print(check_palidrome(user_input))
